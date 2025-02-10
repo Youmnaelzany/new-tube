f372ce24eca7b0ac3a1dfd02182ba5e320b87921
Youmna Ahmed <107641709+Youmnaelzany@users.noreply.github.com>
2025-02-10 20:55:17 +0200

    Update Tailwind CSS integration and enhance UI components with improved styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-   <main className="">
-    Home Page
-   </main>
+    <main className="">
+      <Image src="/logo.svg" alt="Logo" width={50} height={50} />
+      <p className="">NewTube</p>
+    </main>
   );
 }
