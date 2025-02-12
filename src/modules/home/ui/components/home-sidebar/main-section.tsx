144f0494fac67f3367bf29de66b08c1e83a9260e
Youmna Ahmed <107641709+Youmnaelzany@users.noreply.github.com>
2025-02-12 21:00:16 +0200

    Refactor main section to implement sidebar navigation with dynamic menu items

--- src/modules/home/ui/components/home-sidebar/main-section.tsx
+++ src/modules/home/ui/components/home-sidebar/main-section.tsx
@@ -1,9 +1,59 @@
+"use client";
+
+import Link from "next/link";
+
+import { FlameIcon, HomeIcon, PlaySquareIcon } from "lucide-react";
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: HomeIcon,
+    auth: true,
+  },
+  {
+    title: "Subscriptions",
+    url: "/feed/subscriptions",
+    icon: PlaySquareIcon,
+    auth: true,
+  },
+  {
+    title: "Trending",
+    url: "/feed/trending",
+    icon: FlameIcon,
+  },
+];
+
 export const MainSection = () => {
   return (
-    <div className="flex flex-col space-y-2">
-      <div className="flex items-center justify-between">
-        <h1 className="text-2xl font-bold">Home</h1>
-      </div>
-    </div>
+    <SidebarGroup className="flex flex-col space-y-2">
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton
+                tooltip={item.title}
+                asChild
+                isActive={false} // TODO: Change to look at current pathname
+                onClick={() => {}}
+              >
+                <Link href={item.url} className="flex items-center gap-4">
+                  <item.icon />
+                  <span className="text-sm">{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
   );
 };
